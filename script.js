3f5508d271c5e10dae5459ff746eff02e8302a7a
B3rzxd3v <d3vilc0re@gmail.com>
2023-07-03 20:37:32 +0700

    Refactoring with class

--- script.js
+++ script.js
@@ -11,31 +11,70 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition((pos) => {
+class App {
+  #map;
+  #mapEvent;
+
+  constructor() {
+    this._getGeoCoords();
+    inputType.addEventListener('change', this._toggleInput);
+    form.addEventListener('submit', this._newWorkout.bind(this));
+  }
+
+  _getGeoCoords() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), (err) => {
+        alert('Geo error');
+      });
+    }
+  }
+
+  _loadMap(pos) {
     const coords = [pos.coords.latitude, pos.coords.longitude];
 
-    const map = L.map('map').setView(coords, 15);
+    this.#map = L.map('map').setView(coords, 15);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 19,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
-    
-    map.on('click', (e) => {
-      const coord = [e.latlng.lat, e.latlng.lng];
-      const marker = L.marker(coord)
-      .addTo(map)
+    }).addTo(this.#map);
+
+    this.#map.on('click', this._showForm.bind(this));
+  }
+
+  _showForm(e) {
+    this.#mapEvent = e;
+    form.classList.remove('hidden');
+    inputDistance.focus();
+  }
+
+  _toggleInput() {
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  }
+
+  _newWorkout(e) {
+    e.preventDefault();
+    if (e.target.classList.contains('hidden')) return;
+  
+    const className = `${inputType.value}-popup`;
+  
+    const coord = [this.#mapEvent.latlng.lat, this.#mapEvent.latlng.lng];
+    const marker = L.marker(coord)
+      .addTo(this.#map)
       .bindPopup(L.popup({
         maxWidth: 250,
         minWidth: 100,
         autoClose: false,
         closeOnClick: false,
-        className: 'running-popup'
+        className
       }))
       .setPopupContent('Test 123')
       .openPopup();
-    });
-  }, (err) => {
-    alert('Geo error');
-  });
+  
+    inputCadence.value = inputDistance.value = inputDuration.value = inputElevation.value = '';
+    inputType.value = 'running';
+    form.classList.add('hidden');
+  }
 }
+
+const app = new App();
